093d9a03b7765bd1a963603e064e64b23eb39e79
rasla yoosafali <raslayoosafali@youzefs-MacBook-Air.local>
2026-05-10 00:13:31 +0530

    updated mobile slider

--- public/js/script.js
+++ public/js/script.js
@@ -1,70 +1,28 @@
 console.log("D.Laban Loaded");
-
 if(window.innerWidth <= 768){
 
     const sliders = document.querySelectorAll(".products-grid");
 
     sliders.forEach((slider)=>{
 
+        let index = 0;
+
         const products = slider.querySelectorAll(".product");
 
-        let index = 0;
-        let autoSlide;
+        setInterval(()=>{
 
-        function startAutoSlide(){
+            index++;
 
-            autoSlide = setInterval(()=>{
+            if(index >= products.length){
+                index = 0;
+            }
 
-                index++;
+            slider.scrollTo({
+                left: slider.clientWidth * index,
+                behavior: "smooth"
+            });
 
-                if(index >= products.length){
-                    index = 0;
-                }
-
-                slider.scrollTo({
-                    left:index * slider.clientWidth,
-                    behavior:"smooth"
-                });
-
-            },3000);
-
-        }
-
-        function stopAutoSlide(){
-            clearInterval(autoSlide);
-        }
-
-        function resetAutoSlide(){
-
-            stopAutoSlide();
-
-            setTimeout(()=>{
-                startAutoSlide();
-            },3000);
-
-        }
-
-        slider.addEventListener("touchstart",stopAutoSlide);
-
-        slider.addEventListener("touchend",()=>{
-
-            index = Math.round(
-                slider.scrollLeft / slider.clientWidth
-            );
-
-            resetAutoSlide();
-
-        });
-
-        slider.addEventListener("scroll",()=>{
-
-            index = Math.round(
-                slider.scrollLeft / slider.clientWidth
-            );
-
-        });
-
-        startAutoSlide();
+        },3000);
 
     });
 
